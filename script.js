a747ec055b0d659cb584040e7859545cd6a94c4d
naiknawarerb-hub <naiknawarerb@gmail.com>
2026-04-29 15:04:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.contact-form')?.addEventListener('submit', function (event) {
-  event.preventDefault();
-  alert('Thanks for reaching out! We will follow up soon with your digital notes details.');
-  event.target.reset();
-});
